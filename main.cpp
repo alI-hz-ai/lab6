fc17d3094c7950f03d882e2228b07a8471285cb3
m.salomov <m.salomov@newuu.uz>
2025-02-21 10:20:07 +0500

    lab6

---
+++ main.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // prob1a
+    //  int n, sum = 0, i=1;
+    //  cout << "Enter a positive  integer: ";
+    //
+    //  cin >> n;
+    //  while (i <= n)
+    //  {
+    //   sum += i;
+    //   i++;
+    //  }
+    //  cout << "Sum of natural numbers up to " << n<< ": " << sum << endl;
+    // }
+    // b
+    // int n, sum = 0, i=1;
+    // cout << "Enter a positive  integer: ";
+    //
+    // cin >> n;
+    //  for(int i=1; i<=100; i++)
+    // {
+    //  sum += i;
+    //
+    // }
+    // cout << "Sum of natural numbers up to " << n<< ": " << sum << endl;
+    // }
+
+
+    // bool isPrime(int num);
+
+    //  int n, count = 0, num = 2;
+    //
+    //  cout << "Enter how many prime numbers to display: ";
+    //  cin >> n;
+    //
+    //  cout << "First " << n << " prime numbers: ";
+    //  while (count < n) {
+    //   if (isPrime(num)) {
+    //    cout << num << " ";
+    //    count++;
+    //   }
+    //   num++;
+    //  }
+    //
+    //  cout << endl;
+    //  return 0;
+    // }
+    //
+    // bool isPrime(int num) {
+    //  if (num < 2) return false;
+    //  for (int i = 2; i * i <= num; i++) {
+    //   if (num % i == 0) return false;
+    //  }
+    //  return true;
+    // }
+
+
+    // bool isPrime(int num);
+    //
+    //
+    //  int  count = 0, num = 2;
+    //
+    //
+    //
+    //
+    //  cout << "First " << 10 << " prime numbers: ";
+    //  for (int i = 0; i <= 10; ) {
+    //   if (isPrime(num)) {
+    //    cout << num << " ";
+    //    i++;
+    //   }
+    //   num++;
+    //  }
+    //
+    //  cout << endl;
+    //  return 0;
+    // }
+    //
+    // bool isPrime(int num) {
+    //  if (num < 2) return false;
+    //  for (int i = 2; i * i <= num; i++) {
+    //   if (num % i == 0) return false;
+    //  }
+    //  return true;
+    // }
+    // prob3
+    //  int n;
+    //  cout << "Enter a positive integer: ";
+    //  cin>> n;
+    //  if (n <= 0)
+    //  {
+    //   cout << "Please enter a positive integer less than zero";
+    //  } cout << "Collatz sequence: ";
+    //  while (n !=1) {
+    //   cout << n <<" ";
+    //   if ( n % 2 == 0)
+    //   {
+    //    n /=2;
+    //   } else
+    //   {
+    //    n = 3*n +1;
+    //   }
+    //  }
+    //  cout << "1" << endl;
+    // }
+    //  int   n, count = 0;
+    //
+    //  cout << "Enter a positive integer: ";
+    //  cin >> n;
+    //  if ( n <= 0)
+    //  {
+    //   cout << "Please enter a positive integer more than zero";
+    //  }
+    //  while ( n !=0){
+    //   n  /=10;
+    //  count++;
+    // }
+    // cout << "Number of digits: "<< count  << endl;
+    // }
+    //  int   n, count = 0;
+    //
+    //  cout << "Enter a positive integer: ";
+    //  cin >> n;
+    //  if ( n <= 0)
+    //  {
+    //   cout << "Please enter a positive integer more than zero";
+    //  }
+    // cout << "Digits from right to left: ";
+    //  while ( n !=0){
+    //   int digit = n%10;
+    //   cout << digit << " ";
+    //   n  /=10;
+    //
+    // }
+    // cout <<  endl;
+    // }
+//     int a,b;
+//     cout << "Please enter two integers: ";
+//     cin >> a >> b;
+//     if (   a<=0 || b<=0)
+//     {
+//         cout << " Please enter numbers more than zero:: ";
+//     }
+//     while (b !=0)
+//     {
+//         int r = a%b;
+//         a = b;
+//         b=r;
+//     }
+//     cout << " GCD : " << a << endl;
+// }
+
+// prob 7
